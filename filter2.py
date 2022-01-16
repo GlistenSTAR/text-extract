9025c00bce0259e606a261579ac4f53de532133f
GlistenSTAR <glistenstar00@gmail.com>
2022-01-16 21:14:11 +0100

    add filter2.py file for second type

---
+++ filter2.py
@@ -0,0 +1,10 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import simplejson
+import time    
+import re
+
+finialDic=[]
+
+def filter2(all_blocks):
+  print("type2")
