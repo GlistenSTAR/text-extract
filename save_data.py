5312e0d49464e5c56d3a81f78f2042d5c63006ad
GlistenSTAR <glistenstar00@gmail.com>
2022-01-17 07:54:01 +0100

    add second table with second result on save_db function

--- save_data.py
+++ save_data.py
@@ -3,15 +3,11 @@ import sys
 import boto3
 import os
 
-ENDPOINT="textract.czhhj2yyt7he.us-east-2.rds.amazonaws.com"
-PORT="3306"
-USER="admin"
-REGION="us-east-2a"
-DBNAME="textract"
-os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
-passwd = "kCjpSdt5f7s7FQ7CD4kf"
+from env import ENDPOINT, PORT, USER, REGION, DBNAME, passwd
 
-def save_db(all_blocks):
+os.environ['LIBMYSQL_ENABLE_CLEARTEXT_PLUGIN'] = '1'
+
+def save_db(all_blocks, type):
   #gets the credentials from .aws/credentials
   session = boto3.Session(region_name = REGION)
   client = session.client('rds')
@@ -21,8 +17,14 @@ def save_db(all_blocks):
   try:
       conn =  mysql.connector.connect(host=ENDPOINT, user=USER, passwd=passwd, port=PORT, database=DBNAME)
       cur = conn.cursor()
-      myquery = "INSERT INTO Result (Adres, Bouwjaar, Class, Compactheid, Datum_registratie, Energy, Koeling, Ventilatie, Verwarming, Vloeroppervlakte, Isolatie, Warm_water, Woningtype, Zonneboiler, Zonnepanelen) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-      val = (all_blocks['Adres'], all_blocks['Bouwjaar'], all_blocks['Class'], all_blocks['Compactheid'], all_blocks['Datum registratie'], all_blocks['Energy'], all_blocks['Koeling'], all_blocks['Ventilatie'], all_blocks['Verwarming'], all_blocks['Vloeroppervlakte'], str(all_blocks['Isolatie']), all_blocks["Warm water"], all_blocks["Woningtype"], all_blocks["Zonneboiler"], all_blocks["Zonnepanelen"])
+      
+      if(type == "1"):
+        myquery = "INSERT INTO result1 (Adres, Bouwjaar, Class, Compactheid, Datum_registratie, Energy, Koeling, Ventilatie, Verwarming, Vloeroppervlakte, Isolatie, Warm_water, Woningtype, Zonneboiler, Zonnepanelen) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (all_blocks['Adres'], all_blocks['Bouwjaar'], all_blocks['Class'], all_blocks['Compactheid'], all_blocks['Datum registratie'], all_blocks['Energy'], all_blocks['Koeling'], all_blocks['Ventilatie'], all_blocks['Verwarming'], all_blocks['Vloeroppervlakte'], str(all_blocks['Isolatie']), all_blocks["Warm water"], all_blocks["Woningtype"], all_blocks["Zonneboiler"], all_blocks["Zonnepanelen"])
+      elif(type == "2"):
+        myquery = "INSERT INTO result2 (Adres, Aparte_warmtapwatervoorziening, Bouwperiode, Dakisolatie, Datum_van_registratie, Gevelisolatie, Glas_slaapruimte, Glas_woonruimte, Ventilatie, Verwarming, Vloerisolatie, Woningtype, Woonoppervlakte, Zonne_energie, Tip, Energielabel) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (all_blocks['Adres'], all_blocks['Aparte warmtapwatervoorziening'], all_blocks['Bouwperiode'], all_blocks['Dakisolatie'], all_blocks['Datum van registratie'], all_blocks['Gevelisolatie'], all_blocks['Glas slaapruimte'], all_blocks['Glas woonruimte'], all_blocks['Ventilatie'], all_blocks['Verwarming'], all_blocks["Vloerisolatie"],  all_blocks["Woningtype"], all_blocks["Woonoppervlakte"], all_blocks["Zonne-energie"], str(all_blocks['Tip']), all_blocks["Energielabel"])  
+      
       cur.execute(myquery, val)
       conn.commit()
       print("Successfully inserted the db!")
