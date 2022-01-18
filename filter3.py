8ff3dc98f569de60af43780621eab1f3c5ee172b
GlistenSTAR <glistenstar00@gmail.com>
2022-01-18 20:36:07 +0100

    finished third type

--- filter3.py
+++ filter3.py
@@ -6,6 +6,7 @@ import re
 
 finialDic={}
 address1 = address2 = address3 = ''
+advice_for_home = []
 
 def get_bottom_object(item, items, method, size):
     result = ''
@@ -40,12 +41,13 @@ def get_bottom_object(item, items, method, size):
 
 
 def filter3(all_blocks):
+  start_pos = end_pos = 0
   for key, item in enumerate(all_blocks):
     if(all_blocks[key]["Text"] == '(zie toelichting in bijlage)'):
       finialDic["Class"] = all_blocks[key-1]["Text"]
       continue
-    if(all_blocks[key]["Text"] == 'Rijwoning hoek'):
-      finialDic["Rijwoning hoek"] = get_bottom_object(all_blocks[key], all_blocks, "", "md")
+    if(all_blocks[key]["Text"] == 'Labelklasse maakt vergelijking met woning(en) van het volgende type mogelijk.'):
+      finialDic["Label Class"] = all_blocks[key+1]["Text"]
       continue
     if(all_blocks[key]["Text"] == 'Straat'):
       finialDic["Straat"] = get_bottom_object(all_blocks[key], all_blocks, "", "md")
@@ -65,6 +67,27 @@ def filter3(all_blocks):
     if(all_blocks[key]["Text"] == 'Woonplaats'):
       finialDic["Woonplaats"] = get_bottom_object(all_blocks[key], all_blocks, "", "md")
       continue
+    
+    if("(elektriciteit)" in all_blocks[key]["Text"] and not "Elektriciteit" in finialDic):
+      finialDic["Elektriciteit"] = all_blocks[key]["Text"].split("(")[0]
+    if("(gas)" in all_blocks[key]["Text"] and not "Gas" in finialDic):
+      finialDic["Gas"] = all_blocks[key]["Text"].split("(")[0]
+    if("(warmte)" in all_blocks[key]["Text"] and not "Warmte" in finialDic):
+      finialDic["Warmte"] = all_blocks[key]["Text"].split("(")[0]
       
-  print(finialDic)
-  time.sleep(3000)
+    if(all_blocks[key]["Text"] == "Advies voor uw woning" and start_pos == 0):
+        start_pos = key
+    if(all_blocks[key]["Text"] == "BIJLAGE" and end_pos == 0):
+        end_pos = key
+      
+    if(all_blocks[key]["Text"] == "(energie-index)" and not "Energie Index" in finialDic):
+      finialDic["Energie Index"] = all_blocks[key-1]["Text"].split(" ")[-1:][0]
+      
+  for index in range(start_pos, end_pos):
+    if(float(all_blocks[index]["Geometry"]["Polygon"][0]["X"]) > 0.74):
+      advice_for_home.append(all_blocks[index]["Text"])
+  finialDic["Advice"] = advice_for_home
+  
+  # print(finialDic)
+  # time.sleep(3000)
+  return finialDic
--- save_data.py
+++ save_data.py
@@ -24,10 +24,12 @@ def save_db(all_blocks, type):
       elif(type == "2"):
         myquery = "INSERT INTO result2 (Adres, Aparte_warmtapwatervoorziening, Bouwperiode, Dakisolatie, Datum_van_registratie, Gevelisolatie, Glas_slaapruimte, Glas_woonruimte, Ventilatie, Verwarming, Vloerisolatie, Woningtype, Woonoppervlakte, Zonne_energie, Tip, Energielabel, Bag_ID, Registratienummer) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
         val = (all_blocks['Adres'], all_blocks['Aparte warmtapwatervoorziening'], all_blocks['Bouwperiode'], all_blocks['Dakisolatie'], all_blocks['Datum van registratie'], all_blocks['Gevelisolatie'], all_blocks['Glas slaapruimte'], all_blocks['Glas woonruimte'], all_blocks['Ventilatie'], all_blocks['Verwarming'], all_blocks["Vloerisolatie"],  all_blocks["Woningtype"], all_blocks["Woonoppervlakte"], all_blocks["Zonne-energie"], str(all_blocks['Tip']), all_blocks["Energielabel"], all_blocks["Bag_ID"], all_blocks["Registratienummer"])  
-      
+      elif(type == "3"):
+          myquery = "INSERT INTO result3 (Class, Elektriciteit, Energie_Index, Gas, Gebruiksoppervlak, LabelClass, Nummer_toevoeging, Opnamedatum, Postcode, Straat, Warmte, Woonplaats, Advice) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+          val = (all_blocks['Class'], all_blocks['Elektriciteit'], all_blocks['Energie Index'], all_blocks['Gas'], all_blocks['Gebruiksoppervlak'], all_blocks['Label Class'], all_blocks['Nummer/toevoeging'], all_blocks['Opnamedatum'], all_blocks['Postcode'], all_blocks['Straat'], all_blocks['Warmte'], all_blocks["Woonplaats"], str(all_blocks["Advice"]))
       cur.execute(myquery, val)
       conn.commit()
-      print("Successfully inserted the db!")
+      print("Successfully inserted the db!")  
       success = True
   except Exception as e:
       print("Database connection failed due to {}".format(e))  
